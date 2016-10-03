be300718a28589467770fa4d576304e31a8b2cf0
kboom <gurgul.grzegorz@gmail.com>
2016-10-03 21:50:59 +0200

    Cleaning up

--- src/test/groovy/com/kbhit/orangebox/trading/MappingsTest.java
+++ src/test/groovy/com/kbhit/orangebox/trading/MappingsTest.java
@@ -11,24 +11,9 @@ public class MappingsTest extends IntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    public void canSerializeBid() {
-        assertThat(objectMapper.canSerialize(Bid.class)).isTrue();
-    }
-
-    @Test
-    public void canSerializeTrade() {
-        assertThat(objectMapper.canSerialize(Trade.class)).isTrue();
-    }
-
-    @Test
-    public void canSerializeBidder() {
-        assertThat(objectMapper.canSerialize(Bidder.class)).isTrue();
-    }
-
-    @Test
-    public void canSerializeItem() {
-        assertThat(objectMapper.canSerialize(TradedItem.class)).isTrue();
-    }
-
+//    @Test
+//    public void canSerializeBid() {
+//        assertThat(objectMapper.canSerialize(Bid.class)).isTrue();
+//    }
+//
 }
